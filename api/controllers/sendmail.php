7eff566da184ce9c85ed8cd36f2d38647f7b74b7
Maxdarkt <tourneuxmaxence@gmail.com>
2022-04-24 10:38:57 +0200

    test cors

--- api/controllers/sendmail.php
+++ api/controllers/sendmail.php
@@ -1,6 +1,5 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-
 require_once( __DIR__ . '/../../config.php');
 require_once( __DIR__ . '/../../src/PHPMailer-master/src/Exception.php');
 require_once( __DIR__ . '/../../src/PHPMailer-master/src/PHPMailer.php');
