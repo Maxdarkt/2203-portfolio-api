031ef82256545c7704f876544c9c35f3c78951df
Maxdarkt <tourneuxmaxence@gmail.com>
2022-04-25 09:05:22 +0200

    define cors PUT, DELETE

--- cors.php
+++ cors.php
@@ -26,13 +26,11 @@ function cors() {
       
       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
           // may also be using PUT, PATCH, HEAD etc
-          header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+          header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
       
       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
           header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
   
       exit(0);
   }
-  
-  echo "You have CORS!";
 }
