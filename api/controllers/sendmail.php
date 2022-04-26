1d8218dbcab891e282994793cc6831783c1d9610
Maxdarkt <tourneuxmaxence@gmail.com>
2022-04-26 10:34:34 +0200

    test ftp github action

--- api/controllers/sendmail.php
+++ api/controllers/sendmail.php
@@ -11,9 +11,10 @@ function smtpMailer($to, $from, $from_name, $subject, $body) {
 	$mail->IsSMTP(); // active SMTP
 	$mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
 	$mail->SMTPAuth = true;  // Authentification SMTP active
+  $mail->SMTPAutoTLS = false;
 	$mail->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
 	$mail->Host = 'smtp.gmail.com';
-	$mail->Port = 465;
+  $mail->Port = 465;
 	$mail->Username = MAIL_USER;
 	$mail->Password = MAIL_PASS;
 	$mail->SetFrom($from, $from_name);
